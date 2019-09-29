71654817e04793399dd1a9543e172ecbd8e5374f
Marcos Sarges <dev.marcos.sarges@gmail.com>
2019-09-29 15:39:56 -0300

    build

---
+++ build/precache-manifest.85e42f03f9f17d1cbc6444d20be8addd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c9530fb118ba2e5f634f681fc35aa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83ee70b665f05ce2b08",
+    "url": "/static/css/main.c9f6593d.chunk.css"
+  },
+  {
+    "revision": "64ebc6a361c84f0d35d6",
+    "url": "/static/js/2.d7a219b7.chunk.js"
+  },
+  {
+    "revision": "c83ee70b665f05ce2b08",
+    "url": "/static/js/main.3c6b2571.chunk.js"
+  },
+  {
+    "revision": "406a5c76a6044560dfca",
+    "url": "/static/js/runtime-main.8fc68a58.js"
+  },
+  {
+    "revision": "17999a96c5e2f0de699f2bdaee23e5c7",
+    "url": "/static/media/StarJediLogoMonoLine.17999a96.ttf"
+  },
+  {
+    "revision": "2f97edea42d525cd2ee68d5cb6de8d7f",
+    "url": "/static/media/StarJediLogoMonoLine.2f97edea.woff2"
+  },
+  {
+    "revision": "3dce142efbf87c7252132f8f85ddd1aa",
+    "url": "/static/media/StarJediLogoMonoLine.3dce142e.woff"
+  },
+  {
+    "revision": "ba50d6980b5f61a94c36d0bf6646b078",
+    "url": "/static/media/StarJediLogoMonoLine.ba50d698.svg"
+  },
+  {
+    "revision": "ed5fefd0569f2599c03e8bdc953153a6",
+    "url": "/static/media/StarJediLogoMonoLine.ed5fefd0.eot"
+  },
+  {
+    "revision": "3333299897f5d02b9b27283b8fa4c973",
+    "url": "/static/media/bg.33332998.png"
+  },
+  {
+    "revision": "fae719eac2ae3f4af26b97be8910e16b",
+    "url": "/static/media/darth-vader.fae719ea.png"
+  }
+]);
